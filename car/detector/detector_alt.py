f0bd3f206aa394b478bc31738d000e5a7cb84c49
deadlywords <ckreutzer0@googlemail.com>
2022-11-16 00:12:17 +0100

    code cleanup

--- car/detector/detector_alt.py
+++ car/detector/detector_alt.py
@@ -2,7 +2,6 @@
 
 from __future__ import division
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from scipy import interpolate
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -41,17 +40,6 @@ exit_signal = False
 
 pylons = ["blue","green","orange","pink","yellow"]
 
-global_x = []
-global_y = []
-global_xnew = []
-global_ynew = []
-
-
-global_x1 = []
-global_y1 = []
-global_x1new = []
-global_y1new = []
-
 def img_preprocess(img, device, half, net_size):
     net_image, ratio, pad = letterbox(img[:, :, :3], net_size, auto=False)
     net_image = net_image.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
@@ -66,19 +54,7 @@ def img_preprocess(img, device, half, net_size):
     return img, ratio, pad
 
 
-def plotCircle(nodes):
-    x = nodes[:,0]
-    y = nodes[:,1]
-
-    tck,u     = interpolate.splprep( [x,y] ,s = 0) #,per=True 
-    xnew,ynew = interpolate.splev( np.linspace( 0, 1, 100 ), tck,der = 0)
-
-    return [x,y,xnew,ynew]
-    # plt.plot( x,y,'o' , xnew ,ynew )
-
-
-def plotSplines(pylons_left, pylons_right,vis):
-    global global_x1, global_y1, global_x1new, global_y1new, global_x, global_y, global_xnew, global_ynew
+def plotPylons(pylons_left, pylons_right,vis):
     # print(pylons_left)
     # print(pylons_right)
     plt.clf()
@@ -110,6 +86,7 @@ def plotSplines(pylons_left, pylons_right,vis):
 
     temp = merge_image(tempvis,image,1920 - int(680/2),0)
     cv2.imwrite('../../dump/view.jpg', temp)
+    plt.close()
 
 
 def xywh2abcd(xywh, im_shape):
@@ -172,7 +149,6 @@ def torch_thread(weights, img_size, conf_thres=0.2, iou_thres=0.45):
 
 
     # Load model
-    # TODO: attempt_load kills matplot!
     model = attempt_load(weights, device,False)  # load FP32
     bizzi = False
     stride = int(model.stride.max())  # model stride
@@ -199,28 +175,8 @@ def torch_thread(weights, img_size, conf_thres=0.2, iou_thres=0.45):
             run_signal = False
         sleep(0.01)
 
-#matplotlib.use("TkAgg")
 
 fig, ax = plt.subplots()
-
-def animate(i):
-    print("lalalalallalalal")
-    global global_x1, global_y1, global_x1new, global_y1new, global_x, global_y, global_xnew, global_ynew
-    # pullData = open("sampleText.txt","r").read()
-    # dataArray = pullData.split('\n')
-    # xar = []
-    # yar = []
-    # for eachLine in dataArray:
-    #     if len(eachLine)>1:
-    #         x,y = eachLine.split(',')
-    #         xar.append(int(x))
-    #         yar.append(int(y))
-    print("plotaaa")
-    global ax1
-    ax.clear()
-    ax.plot( global_x,global_y,'o' , global_xnew ,global_ynew )
-    ax.plot( global_x1,global_y1,'o' , global_x1new ,global_y1new )
-    ax.set_ylim([0,255])
 
 # https://stackoverflow.com/questions/14063070/overlay-a-smaller-image-on-a-larger-image-python-opencv
 def merge_image(back, front, x,y):
@@ -292,32 +248,8 @@ def plt_thread():
     objects = sl.Objects()
     obj_runtime_param = sl.ObjectDetectionRuntimeParameters()
 
-    #tracking_parameters = sl.PositionalTrackingParameters()
-    #err = zed.enable_positional_tracking(tracking_parameters)
-
     # Display
     camera_infos = zed.get_camera_information()
-    # Create OpenGL viewer
-    # viewer = gl.GLViewer()
-    # point_cloud_res = sl.Resolution(min(camera_infos.camera_resolution.width, 720), min(camera_infos.camera_resolution.height, 404))
-# 
-    # point_cloud_render = sl.Mat()
-    # viewer.init(camera_infos.camera_model, point_cloud_res, obj_param.enable_tracking)
-    # point_cloud = sl.Mat(point_cloud_res.width, point_cloud_res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
-    # image_left = sl.Mat()
-    # # Utilities for 2D display
-    # display_resolution = sl.Resolution(min(camera_infos.camera_resolution.width, 1280), min(camera_infos.camera_resolution.height, 720))
-    # image_scale = [display_resolution.width / camera_infos.camera_resolution.width, display_resolution.height / camera_infos.camera_resolution.height]
-    # image_left_ocv = np.full((display_resolution.height, display_resolution.width, 4), [245, 239, 239, 255], np.uint8)
-# 
-    # # Utilities for tracks view
-    # camera_config = zed.get_camera_information().camera_configuration
-    # tracks_resolution = sl.Resolution(400, display_resolution.height)
-    # track_view_generator = cv_viewer.TrackingViewer(tracks_resolution, camera_config.camera_fps,
-    #                                                 init_params.depth_maximum_distance)
-    # track_view_generator.set_camera_calibration(camera_config.calibration_parameters)
-    # image_track_ocv = np.zeros((tracks_resolution.height, tracks_resolution.width, 4), np.uint8)
-    # Camera pose
     cam_w_pose = sl.Pose()
     print("?")
     while not exit_signal:
@@ -343,120 +275,38 @@ def plt_thread():
             zed.retrieve_objects(objects, obj_runtime_param)
             if objects.is_new :
                 obj_array = objects.object_list
-            #print(str(len(obj_array))+" Object(s) detected\n")
-
-            # print(image_left_tmp)
-
-            # cv2.imwrite('test_raw_left.jpg', image_net)
-            # cv2.imwrite('test_raw_right.jpg', image_net_right)
 
             vis = np.concatenate((image_net, image_net_right), axis=1)
 
             left_pylons = []
             right_pylons = []
             if len(obj_array) > 0:
-                print(len(obj_array))
+                print("Recognized pylons: {0}".format(str(len(obj_array))))
                 for obj in obj_array:
                     if not math.isnan(obj.position[0]):
                         if(obj.raw_label == 0 or obj.raw_label == 1):
                             left_pylons.append([obj.position[0],obj.position[1]])
-                        if(obj.raw_label == 2 or obj.raw_label == 3):
+                        elif(obj.raw_label == 2 or obj.raw_label == 3):
                             right_pylons.append([obj.position[0],obj.position[1]])
-                # print(arr)
-                plotSplines(left_pylons, right_pylons,vis)
-            #print(str(len(obj_array))+" Object(s) detected\n")
-                # # t = time.localtime()
-                # # current_time = time.strftime("%H:%M:%S", t)
-                # first_object = obj_array[0]
-                # zed_pose = sl.Pose()
-                # # print("First object attributes:")
-                # # print(dir(first_object))
-                # print("Label '"+pylons[int(repr(first_object.raw_label))]+"' ("+repr(first_object.raw_label)+") (conf. "+str(int(first_object.confidence))+"/100)")
-                # if obj_param.enable_tracking :
-                #     print(" Tracking ID: "+str(int(first_object.id))+" tracking state: "+repr(first_object.tracking_state)+" / "+repr(first_object.action_state)+" @ "+str(time.time()))
-                # position = first_object.position
-                # velocity = first_object.velocity
-                # dimensions = first_object.dimensions
-                # # print(str(time.time()))
-                # print(" 3D position: [{0},{1},{2}]".format(position[0],position[1],position[2]))
-                # #if zed.grab(runtime_parameters) == SUCCESS :
-                # if True:
-                #     # Get the pose of the camera relative to the world frame
-                #     state = zed.get_position(zed_pose, sl.REFERENCE_FRAME.WORLD)
-                #     # Display translation and timestamp
-                #     py_translation = sl.Translation()
-                #     # tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
-                #     pos = np.subtract(zed_pose.get_translation(py_translation).get(),position)
-                #     if pos[0] > 0:
-                #         print("left @ "+str(round(pos[2],2)))
-                #     else:
-                #         print("right @ "+str(round(pos[2],2)))
-                # #print(np.subtract(zed.get_postition(cam_w_pose, sl.REFERENCE_FRAME.WORLD),position))
-                # # print(" 3D position: [{0},{1},{2}]\n Velocity: [{3},{4},{5}]\n 3D dimentions: [{6},{7},{8}]".format(position[0],position[1],position[2],velocity[0],velocity[1],velocity[2],dimensions[0],dimensions[1],dimensions[2]))
-
-
-
-            # # -- Display
-            # # Retrieve display data
-            # zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA, sl.MEM.CPU, point_cloud_res)
-            # point_cloud.copy_to(point_cloud_render)
-            # zed.retrieve_image(image_left, sl.VIEW.LEFT, sl.MEM.CPU, display_resolution)
-            # zed.get_position(cam_w_pose, sl.REFERENCE_FRAME.WORLD)
-# 
-            # # 3D rendering
-            # viewer.updateData(point_cloud_render, objects)
-            # # 2D rendering
-            # np.copyto(image_left_ocv, image_left.get_data())
-            # cv_viewer.render_2D(image_left_ocv, image_scale, objects, obj_param.enable_tracking)
-            # global_image = cv2.hconcat([image_left_ocv])
-            # # Tracking view
-            # track_view_generator.generate_view(objects, cam_w_pose, image_track_ocv, objects.is_tracked)
-# 
-            # cv2.imshow("ZED | 2D View and Birds View", global_image)
-            # key = cv2.waitKey(10)
-            # if key == 27:
-                # exit_signal = True
+                        else:
+                            print("Found pylon out of identity")
+                    else:
+                        print("Found unpositioned item")
+                plotPylons(left_pylons, right_pylons,vis)
         else:
             print("failed")
-            #exit_signal = True
-    # viewer.exit()
     print("done")
 
-    #exit_signal = True
     zed.close()
 
 def main():
-    global ani
-    ani = "aniiiii"
-    # print("main()")
-    # 
     capture_thread = Thread(target=torch_thread, kwargs={'weights': opt.weights, 'img_size': opt.img_size, "conf_thres": opt.conf_thres})
     capture_thread.start()
-    # print("capture_thread")
 # 
-    # plt_thread()
-
     plot_thread = Thread(target=plt_thread)
     plot_thread.start()
     while bizzi:
-        print("wait")
         time.sleep(1)
-    #ani = animation.FuncAnimation(fig, animate, interval=1000)
-    time.sleep(1)
-
-    #matplotlib.pyplot.plot( [1,1,1],[4,2,3],'o')
-    #plt.show()
-    # print("plot_thread")
-# 
-    #print("plt.hide")
-# 
-    # print("ani")
-
-    # capture_thread.join()
-    # plot_thread.join()
-
-    # plot_thread.join()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
