ef2abbf8828802046bcd56416deb6bfb36fe0f8f
ReDiGermany <support@redigermany.de>
2022-12-16 15:19:49 +0100

    Added dynamic ip loading on windows

--- car/detector/server.py
+++ car/detector/server.py
@@ -286,9 +286,22 @@ class MyServer(http.server.SimpleHTTPRequestHandler):
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 def startServers():
-    nic = "wlan0"
-    # hostName = ni.ifaddresses(nic)[ni.AF_INET][0]['addr']
-    hostName = "192.168.0.29"
+    nic = ""
+    if "wlan0" in ni.interfaces():
+        nic = "wlan0"
+    else:
+        foundIps = []
+        for addr in ni.interfaces():
+            temp = ni.ifaddresses(addr)
+            for n in temp:
+                a = temp[n][0]["addr"]
+                if re.match("(.*)\.(.*)\.(.*)\.(.*)",a) and a!="127.0.0.1":
+                    nic = addr
+                    foundIps.append(a)
+        if len(foundIps)>1:
+            print(f"[WARNING] MULTIPLE IP ADDRESSES FOUND! {json.dumps(foundIps)} - PROBABLY MULTIPLE ACTIVE ADAPTERS! TRY SETTING FIXED IP ON car/detector/server.py:~300")
+    hostName = ni.ifaddresses(nic)[ni.AF_INET][0]['addr']
+    # hostName = "192.168.0.5"
     serverPort = 8080
     print("Using {0}'s ip: {1}".format(nic,hostName))
     
